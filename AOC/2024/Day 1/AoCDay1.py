83cec59657c99c44b0974c447e0efac89b855be4
Ninjauser <brandon.cameron.lee@gmail.com>
2024-12-05 15:22:03 +0000

    Add files via upload

---
+++ AOC/2024/Day 1/AoCDay1.py
@@ -0,0 +1,67 @@
+#AOC Day 1 part 2- hystorian hysteria
+#BCL
+#compare lists and pair numbers from each list with their relatively closest match from the other list. 
+#find the distance between the values and produce a total distance between the lists
+'''
+For example:
+origin:    ->      reorder:   ->   distance:   ->  total:
+3   4              1  3            2               11
+4   3              2  3            1
+2   5              3  3            0
+1   3              3  4            1
+3   9              3  5            2
+3   3              4  9            5
+'''
+
+
+
+
+#variables
+#need to convert the string? 
+listOne = []
+listTwo = []
+distance = []
+dTotal = 0
+
+#open the file to use
+with open("input.txt") as my_file:
+    #print(my_file.readlines())
+
+    for item in my_file.readlines():
+        splitItems = item.split()
+        listOne.append(int(splitItems[0]))
+        listTwo.append(int(splitItems[1]))
+
+print(listOne)
+print(listTwo)
+
+orderOne = sorted(listOne)
+orderTwo = sorted(listTwo)
+print(f"order one:{orderOne}")
+print(f"order two:{orderTwo}")
+
+#encompassed by a loop to go through the list
+#iterate through the list of ordered to calculate the distance between the numbers
+for count, oitem1 in enumerate(orderOne):
+        print(count, oitem1)
+        oitem2 = orderTwo[count]
+        print(oitem2)
+        distance = oitem1 - oitem2
+        dTotal = dTotal + abs(distance)
+
+print(dTotal)
+
+
+
+#for len(distance):
+#totD = distance
+#dtotal = dtotal + totD
+#print(dtotal)
+
+'''
+-DECOMPOSITION-
+take string from file -> split into array ->
+reorder strings -> enumerate for clarity ->
+compare index distance -> running total ->
+output total  distance
+'''
---
+++ AOC/2024/Day 2/AoCDay2.py
@@ -0,0 +1,85 @@
+#Day 2 Red-Nose Reports
+#check the puzzles safety by determining if a report's levels increment or reduce by a "safe" amount
+'''
+must only increase level or decrease level per report but not both
+must only change by at least 1 and at most 3 
+
+example:
+7 6 4 2 1 - safe
+1 2 7 8 9 - unsafe 
+9 7 6 2 1 - unsafe 
+1 3 2 4 5 - unsafe
+8 6 4 4 1 - unsafe 
+1 3 6 7 9 - safe
+
+safe reports = 2
+'''
+#variables
+isIncreasing = False
+safecount = 0
+
+
+#take input and determine if asc/desc
+with open("day2.txt") as file:
+    for reports in file.readlines():
+        #reports is string
+        report = list(map(int,reports.split()))
+        #report is list
+        print(f"report {report}")
+        prev_level = report[0]
+
+
+        if report[1] > prev_level:
+            isIncreasing = True
+        else:
+            isIncreasing = False
+            
+        for level in report[1:]:
+            #print(level)
+            print(f"level {level}, prev_level {prev_level}")
+
+            if isIncreasing == False and level > prev_level or isIncreasing == True and level < prev_level: #if the report isnt increasing or drecreasing go next
+                print("break")
+                break
+            
+            if abs(level - prev_level) >3 or abs(level - prev_level) == 0 : #check if increment is in parameters
+                print("distance break")
+                break
+        
+            prev_level = level
+        else :
+            safecount += 1
+
+print(f"safecount: {safecount}")
+
+
+
+
+#check if whole report follows ascending or descending 
+#for report in isIncreasing:
+    
+
+#increment safe report counter
+
+#output safe reports
+
+
+
+'''
+check if potentially ascending or descending
+->
+continue to check if asc or dsc and increment is safe
+->
+tally safe
+
+'''
+
+
+
+
+
+
+
+
+
+
